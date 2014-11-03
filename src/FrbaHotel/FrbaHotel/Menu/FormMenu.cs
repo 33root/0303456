e7c40ca745e5c7b228119413c40e04f25d885d4d
Esmeralda <ebarbacil@rocketmail.com>
2014-11-03 13:21:33 -0300

    subi mi pantalla porque magicamente habia desaparecido

--- src/FrbaHotel/FrbaHotel/Menu/FormMenu.cs
+++ src/FrbaHotel/FrbaHotel/Menu/FormMenu.cs
@@ -84,7 +84,7 @@ namespace FrbaHotel.Menu
 
         private void button3_Click(object sender, EventArgs e)
         {
-            FrmUsuario b = new FrmUsuario();
+            FrmNuevoUsuario b = new FrmNuevoUsuario();
             this.Hide();
             b.ShowDialog();
             this.Close();
