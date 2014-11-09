54e00f2bd221603a8ecfe3041dab776a7f5579a0
AgustinDavidNievas <AgustinDavidNievas90@gmail.com>
2014-11-09 12:29:03 -0300

    Forms de reserva

---
+++ src/FrbaHotel/FrbaHotel/Generar Modificar Reserva/FormGenerarReserva.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FrbaHotel.Generar_Modificar_Reserva
+{
+    public partial class FormGenerarReserva : Form
+    {
+        public FormGenerarReserva()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkearDisponibilidadButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ingresarButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void verCostoButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
