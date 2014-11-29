1353d1bb976504035a5d0598d25eff36ee96a9b8
AgustinDavidNievas <AgustinDavidNievas90@gmail.com>
2014-11-29 16:33:39 -0300

    Me faltaba un else para mostrar un mensaje cuando si hay disponibilidad

--- src/FrbaHotel/FrbaHotel/Generar Modificar Reserva/FormGenerarReserva.cs
+++ src/FrbaHotel/FrbaHotel/Generar Modificar Reserva/FormGenerarReserva.cs
@@ -26,25 +26,25 @@ namespace FrbaHotel.Generar_Modificar_Reserva
 
         private void checkearDisponibilidadButton_Click(object sender, EventArgs e)
         {
-            string consultaDisponibilidadDeLaReserva = "SELECT ID_Reserva, Fecha_Desde, Cantidad_Huespedes, Cantidad_Noches, ID_Regimen, ID_Habitacion, ID_Cliente"
-                                                              + "FROM AEFI.TL_Reserva r, AEFI.TL_Regimen e, AEFI.TL_Habitacion h"
-                                                              + "WHERE Fecha_Desde = @Fecha_Desde"
-                                                              + "AND Cantidad_Huespedes = @Cantidad_Huespedes"
-                                                              + "AND Cantidad_Noches = @Cantidad_Noches"
+            string consultaDisponibilidadDeLaReserva = "SELECT r.ID_Reserva, r.Fecha_Desde, r.Cantidad_Huespedes, r.Cantidad_Noches, e.ID_Regimen, h.ID_Habitacion, r.ID_Cliente "
+                                                              + "FROM AEFI.TL_Reserva r, AEFI.TL_Regimen e, AEFI.TL_Habitacion h "
+                                                              + "WHERE Fecha_Desde = @Fecha_Desde "
+                                                              + "AND Cantidad_Huespedes = @Cantidad_Huespedes "
+                                                              + "AND Cantidad_Noches = @Cantidad_Noches "
                                                               + "AND ID_Cliente = @ID_Cliente";
                                                               //tengo que ver de donde sacar el ID_Habitacion
 
-            string consultaCantidadDeHuespedesParaLaHabitacion = "SELECT Cantidad_Uespedes_Total"
-                                                               + "FROM AEFI.TL_Tipo_Habitacion h"
-                                                               + "WHERE h.Cantidad_Uespedes_Total = @Cantidad_Uespedes_Total AND h.Descripcion = @Descripcion ";
+            string consultaCantidadDeHuespedesParaLaHabitacion = "SELECT Cantidad_Huespedes_Total "
+                                                               + "FROM AEFI.TL_Tipo_Habitacion "
+                                                               + "WHERE Cantidad_Huespedes_Total = @Cantidad_Huespedes_Total AND Descripcion = @Descripcion ";
 
             try
             {
                 conexion.Open();
                 SqlCommand comando = new SqlCommand(consultaDisponibilidadDeLaReserva, conexion);
-                comando.Parameters.Add(new SqlParameter("@Fecha_Desde", dtpDesde));
-                comando.Parameters.Add(new SqlParameter("@Cantidad_Huespedes", txbCantidadDeHuespedes));
-                comando.Parameters.Add(new SqlParameter("@Cantidad_Noches", txbCantidadDeNoches));
+                comando.Parameters.Add(new SqlParameter("@Fecha_Desde", dtpDesde.Value));//esto no me esta dando bien la fecha
+                comando.Parameters.Add(new SqlParameter("@Cantidad_Huespedes", txbCantidadDeHuespedes.ToString()));
+                comando.Parameters.Add(new SqlParameter("@Cantidad_Noches", txbCantidadDeNoches.ToString()));
                 comando.Parameters.Add(new SqlParameter("@ID_Cliente", Program.idUsuario));
                 //falta que ivan agregue el campu reservada en la tabla de habitaciones, para checkear hay habitaciones del tipo pedido sin reserva
                 SqlDataReader reader = comando.ExecuteReader();
@@ -71,19 +71,26 @@ namespace FrbaHotel.Generar_Modificar_Reserva
                 }
 
                 SqlCommand comando3 = new SqlCommand(consultaCantidadDeHuespedesParaLaHabitacion, conexion);
-                comando3.Parameters.Add(new SqlParameter("@Cantidad_Uespedes_Total", txbCantidadDeHuespedes));
-                comando3.Parameters.Add(new SqlParameter("@Descripcion",cbTipoDeHabitacion));
+                comando3.Parameters.Add(new SqlParameter("@Cantidad_Huespedes_Total", txbCantidadDeHuespedes.ToString()));
+                comando3.Parameters.Add(new SqlParameter("@Descripcion",cbTipoDeHabitacion.SelectedItem.ToString()));
                 SqlDataReader reader3 = comando3.ExecuteReader();
 
-                if(!reader3.HasRows){
+                if(!(reader3.HasRows)){
                     //si no encontro ninguna coincidencia
                     throw new Excepciones("El tipo de habitacion elegido no tiene la capacidad que usted eligio de huespedes");
-                }
+                } else
+                    {
+                        MessageBox.Show("Si hay disponibilidad");   
+                    }
             }
 
             catch (Excepciones exc)
             {
                 MessageBox.Show(exc.Message, "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                conexion.Close();
             }
         }
 
@@ -95,9 +102,9 @@ namespace FrbaHotel.Generar_Modificar_Reserva
             DateTime fechaAcutal = new DateTime();
             comando.CommandType = CommandType.StoredProcedure;
             comando.Parameters.Add(new SqlParameter("@Fecha_Reserva", fechaAcutal.Date));
-            comando.Parameters.Add(new SqlParameter("@Fecha_Desde",dtpDesde));
-            comando.Parameters.Add(new SqlParameter("@Cantidad_Huespedes",txbCantidadDeHuespedes));
-            comando.Parameters.Add(new SqlParameter("@Cantidad_Noches",txbCantidadDeNoches));
+            comando.Parameters.Add(new SqlParameter("@Fecha_Desde",dtpDesde.Value));
+            comando.Parameters.Add(new SqlParameter("@Cantidad_Huespedes",txbCantidadDeHuespedes.ToString()));
+            comando.Parameters.Add(new SqlParameter("@Cantidad_Noches",txbCantidadDeNoches.ToString()));
             if (cbTipoDeRegimen.SelectedItem.ToString() == "Pension Completa")
             {
             comando.Parameters.Add(new SqlParameter("@ID_Regimen",1));
